af5f1115269abb79724d6288003d17c46fb8371f
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-17 13:10:33 +0530

    Add files via upload

---
+++ MethodOverrideEx.java
@@ -0,0 +1,17 @@
+package packagePoly;
+
+import java.lang.reflect.Method;
+
+public class MethodOverrideEx {
+	void method()
+	{
+		System.out.println("methods are members");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+ M.member();
+	}
+
+}
