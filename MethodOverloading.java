8c93e0fc5d7b348e1ed6fce36ea14c7f27d902f0
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-17 13:10:12 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,21 @@
+package packagePoly;
+
+public class MethodOverloading{
+	
+private static void display(int a){
+	System.out.println("Got Integer data.");
+}
+
+//this method accepts string object
+private static void display(String a){
+	System.out.println("Got String object.");
+}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+display(1);
+display("Hello");
+	}
+
+}
